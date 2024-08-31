6acf4769912ee1196a5c9ad718ae115ee7078223
MrVime <mr1234votonmaks@gmail.com>
2024-08-31 20:46:46 +0300

    Add files via upload

--- module_2_5.py
+++ module_2_5.py
@@ -3,9 +3,12 @@ def get_matrix(n,m,value):
     for i in range(n):
         matrix.append([])
         for j in range(m):
-            matrix[i].append(value)
+            if value == 0:
+                continue
+            else:
+                matrix[i].append(value)
     return matrix
-result1 = get_matrix(2,2,10)
+result1 = get_matrix(2,2,0)
 result2 = get_matrix(3,5,42)
 result3 = get_matrix(4,2,13)
 print(result1)
