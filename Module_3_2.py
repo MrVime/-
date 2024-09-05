ba5203e99d113a8e8435f81f43250d41f3e69a82
MrVime <mr1234votonmaks@gmail.com>
2024-09-05 13:17:37 +0300

    Add files via upload

--- Module_3_2.py
+++ Module_3_2.py
@@ -33,16 +33,17 @@ def send_email(message, recipient, *, sender='university.help@gmail.com'):
         if count == 0:
             print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
         else:
-            check_email(sender)
-            if count < 2:
-                print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+            count = 0
+        check_email(sender)
+        if count == 0:
+            print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+        else:
+            if sender == 'university.help@gmail.com':
+                print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
             else:
-                if sender == 'university.help@gmail.com':
-                    print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
-                else:
-                    print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+                print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
     count = 0
-    return
+
 
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
