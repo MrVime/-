ff660db2cae97e0ec368ca71aecf43cd0b5b1432
MrVime <mr1234votonmaks@gmail.com>
2024-10-04 12:49:53 +0300

    Add files via upload

--- Module_3_4.py
+++ Module_3_4.py
@@ -1,31 +1,12 @@
-def check_name(name_1, name_2):
-    list_for_comparison = []
-    count = 0
-    for i in name_1.lower():
-        count += 1
-        if count < len(name_2):
-            if i == name_2[count - 1].lower():
-                list_for_comparison.append(i)
-            else:
-                count = 0
-                continue
-        else:
-            if i == name_2[count - 1]:
-                return True
-
-
 def single_root_words(root_word, *other_words):
     same_words = []
-    comparison = False
     for i in other_words:
-        if len(root_word) < len(i):
-            comparison = check_name(i, root_word)
-            if comparison == True:
-                same_words.append(i)
+        if root_word.lower() in i.lower():
+            same_words.append(i)
+        elif i.lower() in root_word.lower():
+            same_words.append(i)
         else:
-            comparison = check_name(root_word, i)
-            if comparison == True:
-                same_words.append(i)
+            continue
     return same_words
 
 
